f926da37e207507aa22ec60cbc7eefb7e9e5384c
sren <renshs@gmail.com>
2016-07-28 14:24:30 +0000

    move oracle to default dir /home/bin/oracle

--- oracle.sh
+++ oracle.sh
@@ -1,8 +1,8 @@
 echo "set oracle env"
-export ORACLE_BASE=/home/backup/oracle
-export ORACLE_HOME=/home/backup/oracle/product/11.2.0/dbhome
+export ORACLE_BASE=/home/bin/oracle/oracle
+export ORACLE_HOME=/home/bin/oracle/oracle/product/11.2.0/dbhome
 export ORACLE_SID=orcl
-export ORACLE_INVENTORY=/home/backup/oracle/inventory
+export ORACLE_INVENTORY=/home/bin/oracle/oracle/inventory
 export ORACLE_BASE ORACLE_SID ORACLE_HOME
 export PATH=$ORACLE_HOME/bin:$PATH
 export LD_LIBRARY_PATH=$ORACLE_HOME/lib:$LD_LIBRARY_PATH
