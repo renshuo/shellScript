7890f70995935f284af66c1894143dbc78ada688
renshuo <renshs@gmail.com>
2017-12-04 14:35:51 +0800

    remove texlive script, use archlinux texlive

--- xelatex.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-source texlive.env
-mainname=$(basename $1 .tex)
-
-echo "xelatex $mainname.tex"
-echo "okular $mainname.pdf"
-
-xelatex --output-directory=/tmp/ $mainname.tex && okular /tmp/$mainname.pdf
-
