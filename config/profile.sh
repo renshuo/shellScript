d8d2aa4f5d53be467eb8f13fc742ef76b8335f59
renshuo <renshs@gmail.com>
2019-10-18 22:03:26 +0800

    拆分为系统配置和工作配置

---
+++ config/profile.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# 本文件的配置用于修正Linux系统本身的问题
+# work.env用于配置开发环境
+export XBIN=/home/bin/0bin/
+export PATH=$PATH:$XBIN
+
+#alias gst="git status ./"
+#alias work='source /home/bin/0bin/work.env'
+
+export EDITOR="emacsclient "
+
+source $XBIN/gradle.env
+source $XBIN/config/fcitx.env
+source $XBIN/texlive.env
+
+#eval "$(fasd --init auto)"
+
+# export IDEA_JDK=/home/bin/java/jdk11
+
+
