639980567198590e976c4f17a042920aa0cf7f00
renshuo <renshs@gmail.com>
2019-11-29 15:42:12 +0800

    增加linkMaildir文件说明

--- linkMaildir.sh
+++ linkMaildir.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
+# create by sren at 2019-11-29
+
+## isync(mbsync) 同步的文件名如果是中文，会转换为utf-7格式：
+## &V4NXPpCuTvY-
+## 本脚本可以将当前目录下的utf-7格式文件名转换为utf-8格式，并建立软链接
+## 这样就可以在emacs中使用正常的中文文件名了
 
 maildirs=$(ls)
-
 
 for i in $maildirs
 do
