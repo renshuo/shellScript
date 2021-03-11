08a85ccfde5bb49ac592f1a82558bc52dee24e7d
renshuo <renshs@gmail.com>
2021-03-11 11:11:10 +0800

    更新各种编程语言的环境配置

--- config/profile.sh
+++ config/profile.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+### ln -s /home/bin/0bin/config/profile.sh /etc/profile.d/sren.sh
+
 # 本文件的配置用于修正Linux系统本身的问题
 # work.env用于配置开发环境
 export XBIN=/home/bin/0bin/
@@ -9,14 +11,14 @@ export PATH=$PATH:$XBIN
 #alias work='source /home/bin/0bin/work.env'
 
 export EDITOR="emacsclient "
-
-source $XBIN/gradle.env
-source $XBIN/jdk.env
 source $XBIN/config/fcitx5.env
+
+source $XBIN/jdk.env
+source $XBIN/gradle.env
+source $XBIN/scala.env
 source $XBIN/texlive.env
+source $XBIN/julia.env
 
 #eval "$(fasd --init auto)"
 
 # export IDEA_JDK=/home/bin/java/jdk11
-
-
